b35d6329c6abbedc802bcabe947a4a6c611cd4de
Irene Lopez <irene.lopez@tum.de>
2021-11-23 19:53:22 +0100

    added funtion to get specific thread from 4plebs archive

--- archive.py
+++ archive.py
@@ -10,10 +10,21 @@ import requests
 import json
 import time
 
+irrelevant_keys = ['doc_id', 'num', 'subnum', 'op', 'timestamp_expired', 'capcode', 'email',
+                   'name', 'trip', 'poster_hash', 'poster_country', 'sticky', 'locked', 'deleted',
+                   'nimages', 'fourchan_date', 'comment_sanitized', 'comment_processed', 'formatted',
+                   'title_processed', 'name_processed', 'email_processed', 'trip_processed', 
+                   'poster_hash_processed', 'poster_country_name', 'poster_country_name_processed', 
+                   'exif', 'troll_country_code', 'troll_country_name', 'since4pass', 'unique_ips', 
+                   'extra_data', 'media', 'board', 'nreplies']  # may be changed depending on needs
+
+# user specific
+headers = {'User-Agent': 'ABC'}    
+
+
 def search_4plebs(start_date, end_date, board):
     """ Search for posts in specific board, during specific timeframe in the 4plebs archive """
     
-    headers = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:94.0) Gecko/20100101 Firefox/94.0'}    
     url = "http://archive.4plebs.org/_/api/chan/search/?boards={}&start={}&end={}&results=thread&order=asc".format(board, start_date, end_date)
     
     # tries to access endpoint. If exception, wait 5 seconds and try again. Maximum 10 tries.
@@ -31,19 +42,44 @@ def search_4plebs(start_date, end_date, board):
     
     posts = r['0']['posts']  # get posts and ignore metadata
     
-    irrelevant_keys = ['doc_id', 'num', 'subnum', 'op', 'timestamp_expired', 'capcode', 'email',
-                       'name', 'trip', 'poster_hash', 'poster_country', 'sticky', 'locked', 'deleted',
-                       'nimages', 'fourchan_date', 'comment_sanitized', 'comment_processed', 'formatted',
-                       'title_processed', 'name_processed', 'email_processed', 'trip_processed', 
-                       'poster_hash_processed', 'poster_country_name', 'poster_country_name_processed', 
-                       'exif', 'troll_country_code', 'troll_country_name', 'since4pass', 'unique_ips', 
-                       'extra_data', 'media', 'board']  # may be changed depending on needs
-    
     for n in range(len(posts)):
         for key in irrelevant_keys:
             del posts[n][key]   # remove irrelevant entries
     
     return posts
+
+def get_4plebs(board, thread_id):
+    """ Look for specific thread in 4plebs archive. 
+    Returns dict of info about original post and a list of comments """
+    
+    url = "http://archive.4plebs.org/_/api/chan/thread/?board={}&num={}".format(board, thread_id)
+    
+    # tries to access endpoint. If exception, wait 5 seconds and try again. Maximum 10 tries.
+    for tries in range(10):
+        try:
+            r = requests.get(url, headers=headers)
+            r.raise_for_status()
+        
+            r = r.json()
+            break
+        except:
+            time.sleep(5)
+            
+    original = r['{}'.format(thread_id)]['op']
+    
+    for key in irrelevant_keys:
+        del original[key]
+    
+    comments = r['{}'.format(thread_id)]['posts']
+    comments_text = []
+
+    for n in comments.keys():
+        comment = comments[n]['comment']
+        if comment != None:
+            comments_text.append(comments[n]['comment'])        
+
+    return original, comments_text
+    
 
 def main():
     """ Just a test on how this works. """
@@ -58,8 +94,13 @@ def main():
     for n in range(len(posts)):
         print("Date of post {}:".format(n), time.ctime(posts[n]['timestamp']))
  
-    with open("test.json", 'w') as f:
-        json.dump(posts, f)
+    thread_id = posts[0]['thread_num']
+    thread, comments = get_4plebs(board, thread_id)
+    print(thread)
+    print(comments[0])
+    
+#    with open("test.json", 'w') as f:
+#        json.dump(posts, f)
     
 
 if __name__ == "__main__":
