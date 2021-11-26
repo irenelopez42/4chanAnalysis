d6bb5cac1475798e49067b8d8eff1913a74c7cd1
Old Boy <63853802+ga42quy@users.noreply.github.com>
2021-11-26 17:59:48 +0100

    Indentation error...

--- testVM.py
+++ testVM.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
- # -*- coding: utf-8 -*-
- """Simply write a file to check, if the VM is configured correctly on startup"""
- with open("hello.txt", "w") as file:
-   file.write("Hello!")
+# -*- coding: utf-8 -*-
+"""Simply write a file to check, if the VM is configured correctly on startup"""
+with open("hello.txt", "w") as file:
+  file.write("Hello!")
