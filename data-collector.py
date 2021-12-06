ffa88bd666f456ff90bb30099fb5b06ed39aaaf3
Irene Lopez <irene.lopez@tum.de>
2021-12-06 17:49:48 +0100

    added support to obtain data from the adv board

--- data-collector.py
+++ data-collector.py
@@ -5,9 +5,10 @@ Created on Tue Nov 23 20:37:32 2021
 
 @author: Irene Lopez Gutierrez
 
-Collect random /pol/ posts through time using the 4plebs API 
+Collect random posts through time using the 4plebs API. Works for two boards: 'pol' and 'adv'.
 """
 
+import sys
 import requests
 import json
 import time
@@ -19,33 +20,44 @@ from archive import get_4plebs
 # user specific
 headers = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:94.0) Gecko/20100101 Firefox/94.0'}   
 
-# some info about 4Plebs
-total_pages = 821178
-post_per_page = 10
-
-def main():
+def main(board):
     
     # create empty dictionary with entries for each month of archive content
     dict_ids = {}
     
-    dict_ids["2013"] = {"Nov": [], "Dec": []}
+    if board == 'pol':
     
-    for year in range(2014, 2021):
-        dict_ids[ f"{year}" ] = {"Jan": [], "Feb": [], "Mar": [], "Apr": [], 
-                "May": [], "Jun": [], "Jul": [], "Aug": [], "Sep": [], 
-                "Oct": [], "Nov": [], "Dec": []}
+        dict_ids["2013"] = {"Nov": [], "Dec": []}
         
-    dict_ids["2021"] = {"Jan": [], "Feb": [], "Mar": [], "Apr": [], 
-                "May": [], "Jun": [], "Jul": [], "Aug": [], "Sep": [], 
-                "Oct": [], "Nov": []}
-    
+        for year in range(2014, 2022):
+            dict_ids[ f"{year}" ] = {"Jan": [], "Feb": [], "Mar": [], "Apr": [], 
+                    "May": [], "Jun": [], "Jul": [], "Aug": [], "Sep": [], 
+                    "Oct": [], "Nov": [], "Dec": []}   
+            
+        # some info about the 4Plebs archive for /pol/
+        total_pages = 821178
+        post_per_page = 10
+        
+        
+    elif board == 'adv':
+        for year in range(2014, 2022):
+            dict_ids[ f"{year}" ] = {"Jan": [], "Feb": [], "Mar": [], "Apr": [], 
+                    "May": [], "Jun": [], "Jul": [], "Aug": [], "Sep": [], 
+                    "Oct": [], "Nov": [], "Dec": []}
+            
+        # some info about the 4Plebs archive for /adv/
+        total_pages = 86415
+        post_per_page = 10
+        
+    else:
+        sys.exit("This board is not supported")
     
     # create empty dict for each year
     years = [{}, {}, {}, {}, {}, {}, {}, {}, {}]
     
     for pag in range(1, total_pages + 1, 80):
         
-        url = f"http://archive.4plebs.org/_/api/chan/index/?board=pol&page={pag}&order=by_thread"
+        url = f"http://archive.4plebs.org/_/api/chan/index/?board={board}&page={pag}&order=by_thread"
 
         # tries to acces url. If access denied, sleep 5 seconds. Maximum tries = 100
         for tries in range(100):
@@ -58,7 +70,7 @@ def main():
             except:
                 time.sleep(5)
  
-        rand_post = list(r.keys())[np.random.randint(0,10)]        
+        rand_post = list(r.keys())[np.random.randint(0, post_per_page)]        
         timestamp = r[rand_post]['op']['timestamp']
         thread_id = r[rand_post]['op']['thread_num']
 
@@ -74,21 +86,24 @@ def main():
     
         ## save json every 11 threads added, as a checkpoint in case of errors
         if not pag % 11:
-            with open(f"dict_ids_{gethostname()}.json", 'w', encoding="utf8") as file:
+            with open(f"Data/{board}/dict_ids_{gethostname()}.json", 'w', encoding="utf8") as file:
                 json.dump(dict_ids, file)
             for year in range(2013,2022):
-                with open(f"year{year}_{gethostname()}.json", 'w', encoding="utf8") as file:
+                with open(f"Data/{board}/year{year}_{gethostname()}.json", 'w', encoding="utf8") as file:
                     json.dump(years[year-2013], file)
             
             
-    with open(f"dict_ids_{gethostname()}.json", 'w', encoding="utf8") as file:
+    with open(f"Data/{board}/dict_ids_{gethostname()}.json", 'w', encoding="utf8") as file:
         json.dump(dict_ids, file)
         
     for year in range(2013,2022):
-        with open(f"year{year}_{gethostname()}.json", 'w', encoding="utf8") as file:
+        with open(f"Data/{board}/year{year}_{gethostname()}.json", 'w', encoding="utf8") as file:
             print("\n TEST \n", years[year-2013])
             json.dump(years[year-2013], file)
 
     
 if __name__ == "__main__":
-    main()
+    
+    board = "pol"  # board abbreviation from which to obtain data
+    
+    main(board)
